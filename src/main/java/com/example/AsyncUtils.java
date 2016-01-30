96f79aed5a5fcf4ef3c98b7e9d28acd206d6edaa
Dave Syer <dsyer@pivotal.io>
2016-01-30 16:24:02 +0000

    Remove guava usage

--- src/main/java/com/example/AsyncUtils.java
+++ src/main/java/com/example/AsyncUtils.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.google.common.base.Throwables;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
@@ -8,17 +7,19 @@ import java.util.concurrent.TimeUnit;
  * Created by dev on 11/23/15.
  */
 public class AsyncUtils {
-  public static int randomSleep(int duration, TimeUnit timeUnit) {
-    int sleepTime = ThreadLocalRandom.current().nextInt(duration);
-    try {
-      timeUnit.sleep(sleepTime);
-    } catch (InterruptedException e) {
-      Throwables.propagate(e);
-    }
-    return sleepTime;
-  }
+	public static int randomSleep(int duration, TimeUnit timeUnit) {
+		int sleepTime = ThreadLocalRandom.current().nextInt(duration);
+		try {
+			timeUnit.sleep(sleepTime);
+		}
+		catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new RuntimeException("Interrupted");
+		}
+		return sleepTime;
+	}
 
-  public static String getThreadName() {
-    return Thread.currentThread().getName();
-  }
+	public static String getThreadName() {
+		return Thread.currentThread().getName();
+	}
 }
